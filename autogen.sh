5d76a8f5c68a3f19fae41489769c8346b16050a8
Wind Yuan <feng.yuan@intel.com>
2015-01-15 07:34:28 -0500

    submodule: add atomisp headers submodule
     atomisp headers commit id "27fd906"
     directory ext/atomisp

--- autogen.sh
+++ autogen.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
+echo "git submodule update"
+git submodule sync
+git submodule init
+git submodule update
+
 echo "Generating configure files"
 autoreconf -i
 # Run twice to get around a "ltmain.sh" bug
