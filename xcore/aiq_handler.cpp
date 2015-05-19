f717fcefcede998e6890da44f7657c307cf4042b
Jia Meng <jia.meng@intel.com>
2015-05-19 10:15:32 +0000

    aiq_handler: apply manual wb gains under manual mode only

--- xcore/aiq_handler.cpp
+++ xcore/aiq_handler.cpp
@@ -1482,17 +1482,19 @@ XCamReturn AiqCompositor::integrate (X3aResultList &results)
     }
     _pa_result = pa_result;
 
-    if (XCAM_DOUBLE_EQUAL_AROUND (aiq_awb->_params.gr_gain, 0.0) ||
-            XCAM_DOUBLE_EQUAL_AROUND (aiq_awb->_params.r_gain, 0.0)  ||
-            XCAM_DOUBLE_EQUAL_AROUND (aiq_awb->_params.b_gain, 0.0)  ||
-            XCAM_DOUBLE_EQUAL_AROUND (aiq_awb->_params.gb_gain, 0.0)) {
-        XCAM_LOG_DEBUG ("Zero gain would cause ISP division fatal error");
-    }
-    else {
-        _pa_result->color_gains[0] = aiq_awb->_params.gr_gain;
-        _pa_result->color_gains[1] = aiq_awb->_params.r_gain;
-        _pa_result->color_gains[2] = aiq_awb->_params.b_gain;
-        _pa_result->color_gains[3] = aiq_awb->_params.gb_gain;
+    if (aiq_awb->get_mode_unlock () == XCAM_AWB_MODE_MANUAL) {
+        if (XCAM_DOUBLE_EQUAL_AROUND (aiq_awb->_params.gr_gain, 0.0) ||
+                XCAM_DOUBLE_EQUAL_AROUND (aiq_awb->_params.r_gain, 0.0)  ||
+                XCAM_DOUBLE_EQUAL_AROUND (aiq_awb->_params.b_gain, 0.0)  ||
+                XCAM_DOUBLE_EQUAL_AROUND (aiq_awb->_params.gb_gain, 0.0)) {
+            XCAM_LOG_DEBUG ("Zero gain would cause ISP division fatal error");
+        }
+        else {
+            _pa_result->color_gains[0] = aiq_awb->_params.gr_gain;
+            _pa_result->color_gains[1] = aiq_awb->_params.r_gain;
+            _pa_result->color_gains[2] = aiq_awb->_params.b_gain;
+            _pa_result->color_gains[3] = aiq_awb->_params.gb_gain;
+        }
     }
 
     isp_params.frame_use = _frame_use;
