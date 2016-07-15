0f66be85297a954f1aeca12dcfeb972d7ae655f1
Wind Yuan <feng.yuan@intel.com>
2016-07-15 06:24:22 -0400

    xcore: add image file handle for image read/write
    Signed-off-by: Wind Yuan <feng.yuan@intel.com>

---
+++ xcore/image_file_handle.cpp
@@ -0,0 +1,127 @@
+/*
+ * image_file_handle.h - Image file handle
+ *
+ *  Copyright (c) 2016 Intel Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Author: Wind Yuan <feng.yuan@intel.com>
+ */
+
+#include "image_file_handle.h"
+
+namespace XCam {
+
+ImageFileHandle::~ImageFileHandle ()
+{
+    close ();
+}
+
+XCamReturn
+ImageFileHandle::open (const char *name, const char *option)
+{
+    XCAM_ASSERT (name);
+    if (!name)
+        return XCAM_RETURN_ERROR_FILE;
+
+    close ();
+    XCAM_ASSERT (!_file_name && !_fp);
+    _fp = fopen (name, option);
+
+    if (!_fp)
+        return XCAM_RETURN_ERROR_FILE;
+    _file_name = strndup (name, 512);
+    return XCAM_RETURN_NO_ERROR;
+}
+
+XCamReturn
+ImageFileHandle::close ()
+{
+    if (_fp) {
+        fclose (_fp);
+        _fp = NULL;
+    }
+    if (_file_name) {
+        xcam_free (_file_name);
+        _file_name = NULL;
+    }
+    return XCAM_RETURN_NO_ERROR;
+}
+
+bool
+ImageFileHandle::end_of_file()
+{
+    if (!is_valid ())
+        return true; // maybe false?
+
+    return feof (_fp);
+}
+
+XCamReturn
+ImageFileHandle::read_buf (SmartPtr<BufferProxy> &buf)
+{
+    const VideoBufferInfo info = buf->get_video_info ();
+    VideoBufferPlanarInfo planar;
+    uint8_t *memory = NULL;
+    XCamReturn ret = XCAM_RETURN_NO_ERROR;
+
+    XCAM_ASSERT (is_valid ());
+
+    memory = buf->map ();
+    for (uint32_t index = 0; index < info.components; index++) {
+        info.get_planar_info (planar, index);
+        uint32_t line_bytes = planar.width * planar.pixel_bytes;
+
+        for (uint32_t i = 0; i < planar.height; i++) {
+            if (fread (memory + info.offsets [index] + i * info.strides [index], 1, line_bytes, _fp) != line_bytes) {
+                if (end_of_file ())
+                    ret = XCAM_RETURN_BYPASS;
+                else {
+                    XCAM_LOG_ERROR ("read file failed, size doesn't match");
+                    ret = XCAM_RETURN_ERROR_FILE;
+                }
+            }
+        }
+    }
+    buf->unmap ();
+    return ret;
+}
+
+XCamReturn
+ImageFileHandle::write_buf (SmartPtr<BufferProxy> buf)
+{
+    const VideoBufferInfo info = buf->get_video_info ();
+    VideoBufferPlanarInfo planar;
+    uint8_t *memory = NULL;
+    XCamReturn ret = XCAM_RETURN_NO_ERROR;
+
+    XCAM_ASSERT (is_valid ());
+
+    memory = buf->map ();
+    for (uint32_t index = 0; index < info.components; index++) {
+        info.get_planar_info (planar, index);
+        uint32_t line_bytes = planar.width * planar.pixel_bytes;
+
+        for (uint32_t i = 0; i < planar.height; i++) {
+            if (fwrite (memory + info.offsets [index] + i * info.strides [index], 1, line_bytes, _fp) != line_bytes) {
+                XCAM_LOG_ERROR ("read file failed, size doesn't match");
+                ret = XCAM_RETURN_ERROR_FILE;
+            }
+        }
+    }
+    buf->unmap ();
+    return ret;
+}
+
+
+}
---
+++ xcore/image_file_handle.h
@@ -0,0 +1,53 @@
+/*
+ * image_file_handle.h - Image file handle
+ *
+ *  Copyright (c) 2016 Intel Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Author: Wind Yuan <feng.yuan@intel.com>
+ */
+
+#ifndef XCAM_IMAGE_FILE_HANDLE_H
+#define XCAM_IMAGE_FILE_HANDLE_H
+
+#include "xcam_utils.h"
+#include "buffer_pool.h"
+
+namespace XCam {
+
+class ImageFileHandle {
+public:
+    ImageFileHandle ()
+        : _fp (NULL)
+        , _file_name (NULL)
+    {}
+    ~ImageFileHandle ();
+
+    bool is_valid () const {
+        return (_fp ? true : false);
+    }
+    bool end_of_file ();
+    XCamReturn open (const char *name, const char *option);
+    XCamReturn close ();
+    XCamReturn read_buf (SmartPtr<BufferProxy> &buf);
+    XCamReturn write_buf (SmartPtr<BufferProxy> buf);
+
+private:
+    FILE   *_fp;
+    char   *_file_name;
+};
+
+}
+
+#endif  //XCAM_IMAGE_FILE_HANDLE_H
