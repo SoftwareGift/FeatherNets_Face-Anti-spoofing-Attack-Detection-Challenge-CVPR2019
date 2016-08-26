515bf82fae90f7babed70ca38b9840039d2f81f7
Wind Yuan <feng.yuan@intel.com>
2016-08-26 02:58:56 -0400

    ocl: add utilities for easy use
     * add write_image in cl_utils.h
    
    Signed-off-by: Wind Yuan <feng.yuan@intel.com>

---
+++ modules/ocl/cl_utils.cpp
@@ -0,0 +1,57 @@
+/*
+ * cl_utils.cpp - CL Utilities
+ *
+ *  Copyright (c) 2016 Intel Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Author: Wind Yuan <feng.yuan@intel.com>
+ */
+
+#include "cl_utils.h"
+
+namespace XCam {
+
+bool
+write_image (SmartPtr<CLImage> image, const char *file_name)
+{
+    XCAM_ASSERT (file_name);
+
+    const CLImageDesc &desc = image->get_image_desc ();
+    void *ptr = NULL;
+    size_t origin[3] = {0, 0, 0};
+    size_t region[3] = {desc.width, desc.height, 1};
+    size_t row_pitch;
+    size_t slice_pitch;
+
+    XCamReturn ret = image->enqueue_map (ptr, origin, region, &row_pitch, &slice_pitch, CL_MEM_READ_ONLY);
+    XCAM_ASSERT (ret == XCAM_RETURN_NO_ERROR);
+    XCAM_ASSERT (ptr);
+    XCAM_ASSERT (row_pitch == desc.row_pitch);
+    uint8_t *buf_start = (uint8_t *)ptr;
+    uint32_t width = image->get_pixel_bytes () * desc.width;
+
+    FILE *fp = fopen (file_name, "wb");
+    XCAM_FAIL_RETURN (ERROR, fp, false, "open file(%s) failed", file_name);
+
+    for (uint32_t i = 0; i < desc.height; ++i) {
+        uint8_t *buf_line = buf_start + row_pitch * i;
+        fwrite (buf_line, width, 1, fp);
+    }
+    image->enqueue_unmap (ptr);
+    fclose (fp);
+    XCAM_LOG_INFO ("write image:%s\n", file_name);
+    return true;
+}
+
+}
---
+++ modules/ocl/cl_utils.h
@@ -0,0 +1,35 @@
+/*
+ * cl_utils.h - CL Utilities
+ *
+ *  Copyright (c) 2016 Intel Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Author: Wind Yuan <feng.yuan@intel.com>
+ */
+
+#ifndef XCAM_CL_UTILS_H
+#define XCAM_CL_UTILS_H
+
+#include "xcam_utils.h"
+#include "cl_context.h"
+#include "cl_memory.h"
+
+namespace XCam {
+
+bool write_image (SmartPtr<CLImage> image, const char *file_name);
+
+}
+
+#endif //XCAM_CL_UTILS_H
+
