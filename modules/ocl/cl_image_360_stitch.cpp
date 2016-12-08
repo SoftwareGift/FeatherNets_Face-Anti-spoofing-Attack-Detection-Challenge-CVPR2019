c9e84b47af5e6634ec7fd2d7ba830b75d8501760
Wind Yuan <feng.yuan@intel.com>
2016-12-08 03:02:35 -0500

    cl_360_stitch: enable 360 image stitching
    Signed-off-by: Wind Yuan <feng.yuan@intel.com>

---
+++ modules/ocl/cl_image_360_stitch.cpp
@@ -0,0 +1,203 @@
+/*
+ * cl_image_360_stitch.cpp - CL Image 360 stitch
+ *
+ *  Copyright (c) 2016 Intel Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Author: Wind Yuan <feng.yuan@intel.com>
+ */
+
+#include "cl_image_360_stitch.h"
+
+namespace XCam {
+
+CLImage360Stitch::CLImage360Stitch ()
+    : CLMultiImageHandler ("CLImage360Stitch")
+    , _output_width (0)
+    , _output_height (0)
+{
+}
+
+bool
+CLImage360Stitch::set_left_blender (SmartPtr<CLBlender> blender)
+{
+    _left_blender = blender;
+
+    SmartPtr<CLImageHandler> handler = blender;
+    return add_image_handler (handler);
+}
+
+bool
+CLImage360Stitch::set_right_blender (SmartPtr<CLBlender> blender)
+{
+    _right_blender = blender;
+
+    SmartPtr<CLImageHandler> handler = blender;
+    return add_image_handler (handler);
+}
+
+bool
+CLImage360Stitch::set_image_overlap (const int idx, const Rect &overlap0, const Rect &overlap1)
+{
+    XCAM_ASSERT (idx < ImageIdxCount);
+    _overlaps[idx][0] = overlap0;
+    _overlaps[idx][1] = overlap1;
+    return true;
+}
+
+XCamReturn
+CLImage360Stitch::prepare_buffer_pool_video_info (
+    const VideoBufferInfo &input,
+    VideoBufferInfo &output)
+{
+    uint32_t output_width = _output_width;
+    uint32_t output_height = _output_height;
+
+    XCAM_FAIL_RETURN(
+        WARNING,
+        output_width && output_height,
+        XCAM_RETURN_ERROR_PARAM,
+        "CLImage360Stitch(%s) prepare buffer pool info failed since width:%d height:%d was not set correctly",
+        XCAM_STR(get_name()), output_width, output_height);
+
+    // aligned at least XCAM_BLENDER_ALIGNED_WIDTH
+    uint32_t aligned_width = XCAM_MAX (16, XCAM_BLENDER_ALIGNED_WIDTH);
+    output.init (
+        input.format, output_width, output_height,
+        XCAM_ALIGN_UP(output_width, aligned_width), XCAM_ALIGN_UP(output_height, 16));
+    return XCAM_RETURN_NO_ERROR;
+}
+
+XCamReturn
+CLImage360Stitch::prepare_blender_parameters (
+    SmartPtr<DrmBoBuffer> &input0, SmartPtr<DrmBoBuffer> &input1, SmartPtr<DrmBoBuffer> &output)
+{
+    XCamReturn ret = XCAM_RETURN_NO_ERROR;
+    const VideoBufferInfo &in0_info = input0->get_video_info ();
+    const VideoBufferInfo &in1_info = input1->get_video_info ();
+    const VideoBufferInfo &out_info = output->get_video_info ();
+
+    XCAM_ASSERT (in0_info.height == in1_info.height);
+    XCAM_ASSERT (in0_info.width <= out_info.width && in1_info.width <= out_info.width);
+
+    Rect main_left = get_image_overlap (ImageIdxMain, 0);
+    Rect main_right = get_image_overlap (ImageIdxMain, 1);
+    Rect scnd_left = get_image_overlap (ImageIdxSecondary, 1);
+    Rect scnd_right = get_image_overlap (ImageIdxSecondary, 0);
+    int main_mid = XCAM_ALIGN_DOWN (in0_info.width / 2, XCAM_BLENDER_ALIGNED_WIDTH);
+    int scnd_mid = 0;
+    int out_mid = XCAM_ALIGN_DOWN (out_info.width / 2, XCAM_BLENDER_ALIGNED_WIDTH);
+    Rect area, out_merge_window;
+    area.pos_y = out_merge_window.pos_y = 0;
+    area.height = out_merge_window.pos_y = out_info.height;
+
+    //calculate left stitching area(input)
+    int32_t prev_pos = main_left.pos_x;
+    main_left.pos_x = XCAM_ALIGN_AROUND (main_left.pos_x, XCAM_BLENDER_ALIGNED_WIDTH);
+    main_left.width = XCAM_ALIGN_UP (main_left.width, XCAM_BLENDER_ALIGNED_WIDTH);
+    scnd_left.pos_x += main_left.pos_x - prev_pos;
+    scnd_left.pos_x = XCAM_ALIGN_AROUND (scnd_left.pos_x, XCAM_BLENDER_ALIGNED_WIDTH);
+    scnd_left.width = main_left.width;
+
+    //calculate right stitching area(input)
+    prev_pos = main_right.pos_x;
+    main_right.pos_x = XCAM_ALIGN_AROUND (main_right.pos_x, XCAM_BLENDER_ALIGNED_WIDTH);
+    main_right.width = XCAM_ALIGN_UP (main_right.width, XCAM_BLENDER_ALIGNED_WIDTH);
+    scnd_right.pos_x += main_right.pos_x - prev_pos;
+    scnd_right.pos_x = XCAM_ALIGN_AROUND (scnd_right.pos_x, XCAM_BLENDER_ALIGNED_WIDTH);
+    scnd_right.width = main_right.width;
+
+    //find scnd_mid
+    scnd_mid = scnd_left.pos_x + (main_mid - main_left.pos_x) - out_mid;
+    if (scnd_mid < scnd_right.pos_x + scnd_right.width)
+        scnd_mid = scnd_right.pos_x + scnd_right.width;
+
+    // set left blender
+    area.pos_x = scnd_mid;
+    area.width = scnd_left.pos_x + scnd_left.width - scnd_mid;
+    _left_blender->set_input_valid_area (area, 0);
+
+    area.pos_x = main_left.pos_x;
+    area.width = main_mid - main_left.pos_x;
+    _left_blender->set_input_valid_area (area, 1);
+
+    out_merge_window.width = main_left.width;
+    out_merge_window.pos_x = out_mid - (main_mid - main_left.pos_x);
+    _left_blender->set_merge_window (out_merge_window);
+    _left_blender->set_input_merge_area (scnd_left, 0);
+    _left_blender->set_input_merge_area (main_left, 1);
+
+    // set right blender
+    area.pos_x = main_mid;
+    area.width = main_right.pos_x + main_right.width - main_mid;
+    _right_blender->set_input_valid_area (area, 0);
+
+    area.pos_x = scnd_right.pos_x;
+    area.width = scnd_mid - scnd_right.pos_x;
+    _right_blender->set_input_valid_area (area, 1);
+
+    out_merge_window.pos_x = out_mid + (main_right.pos_x - main_mid);
+    out_merge_window.width = main_right.width;
+    _right_blender->set_merge_window (out_merge_window);
+    _right_blender->set_input_merge_area (main_right, 0);
+    _right_blender->set_input_merge_area (scnd_right, 1);
+
+    return ret;
+}
+
+XCamReturn
+CLImage360Stitch::prepare_parameters (SmartPtr<DrmBoBuffer> &input, SmartPtr<DrmBoBuffer> &output)
+{
+    SmartPtr<DrmBoBuffer> input1 = input->find_typed_attach<DrmBoBuffer> ();
+    XCAM_FAIL_RETURN(
+        WARNING,
+        input1.ptr (),
+        XCAM_RETURN_ERROR_PARAM,
+        "CLImage360Stitch(%s) does NOT find second buffer in attachment", get_name());
+
+    XCamReturn ret = prepare_blender_parameters (input, input1, output);
+    XCAM_FAIL_RETURN(
+        WARNING,
+        ret == XCAM_RETURN_NO_ERROR,
+        XCAM_RETURN_ERROR_PARAM,
+        "CLImage360Stitch(%s) failed to prepare blender parameters", get_name());
+
+    return CLMultiImageHandler::prepare_parameters (input, output);
+}
+
+SmartPtr<CLImageHandler>
+create_image_360_stitch (SmartPtr<CLContext> &context, bool need_seam)
+{
+    const int layer = 2;
+    const bool need_uv = true;
+    SmartPtr<CLBlender>  left_blender, right_blender;
+    SmartPtr<CLImage360Stitch> stitch = new CLImage360Stitch ();
+    XCAM_ASSERT (stitch.ptr ());
+
+    left_blender = create_pyramid_blender (context, layer, need_uv, need_seam).dynamic_cast_ptr<CLBlender> ();
+    XCAM_FAIL_RETURN (ERROR, left_blender.ptr (), NULL, "image_360_stitch create left blender failed");
+    left_blender->disable_buf_pool (true);
+    left_blender->swap_input_idx (true);
+    stitch->set_left_blender (left_blender);
+
+    right_blender = create_pyramid_blender (context, layer, need_uv, need_seam).dynamic_cast_ptr<CLBlender> ();
+    XCAM_FAIL_RETURN (ERROR, right_blender.ptr (), NULL, "image_360_stitch create right blender failed");
+    right_blender->disable_buf_pool (true);
+    stitch->set_right_blender (right_blender);
+
+    return stitch;
+}
+
+}
+
---
+++ modules/ocl/cl_image_360_stitch.h
@@ -0,0 +1,80 @@
+/*
+ * cl_image_360_stitch.h - CL Image 360 stitch
+ *
+ *  Copyright (c) 2016 Intel Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Author: Wind Yuan <feng.yuan@intel.com>
+ */
+
+#ifndef XCAM_CL_IMAGE_360_STITCH_H
+#define XCAM_CL_IMAGE_360_STITCH_H
+
+#include "xcam_utils.h"
+#include "cl_multi_image_handler.h"
+#include "cl_blender.h"
+
+namespace XCam {
+
+class CLImage360Stitch
+    : public CLMultiImageHandler
+{
+public:
+    enum ImageIdx {
+        ImageIdxMain,
+        ImageIdxSecondary,
+        ImageIdxCount,
+    };
+public:
+    explicit CLImage360Stitch ();
+
+    void set_output_size (uint32_t width, uint32_t height) {
+        _output_width = width; //XCAM_ALIGN_UP (width, XCAM_BLENDER_ALIGNED_WIDTH);
+        _output_height = height;
+    }
+    bool set_left_blender (SmartPtr<CLBlender> blender);
+    bool set_right_blender (SmartPtr<CLBlender> blender);
+
+    bool set_image_overlap (const int idx, const Rect &overlap0, const Rect &overlap1);
+
+    const Rect &get_image_overlap (ImageIdx image, int num) {
+        XCAM_ASSERT (image < ImageIdxCount && num < 2);
+        return _overlaps[image][num];
+    }
+
+protected:
+    virtual XCamReturn prepare_buffer_pool_video_info (
+        const VideoBufferInfo &input,
+        VideoBufferInfo &output);
+    virtual XCamReturn prepare_parameters (SmartPtr<DrmBoBuffer> &input, SmartPtr<DrmBoBuffer> &output);
+
+    XCamReturn prepare_blender_parameters (
+        SmartPtr<DrmBoBuffer> &input0, SmartPtr<DrmBoBuffer> &input1, SmartPtr<DrmBoBuffer> &output);
+
+    XCAM_DEAD_COPY (CLImage360Stitch);
+
+private:
+    SmartPtr<CLBlender>    _left_blender;
+    SmartPtr<CLBlender>    _right_blender;
+    uint32_t               _output_width;
+    uint32_t               _output_height;
+    Rect                   _overlaps[ImageIdxCount][2];   // 2=>Overlap0 and overlap1
+};
+
+SmartPtr<CLImageHandler>
+create_image_360_stitch (SmartPtr<CLContext> &context, bool need_seam = false);
+
+}
+
+#endif //XCAM_CL_IMAGE_360_STITCH_H
