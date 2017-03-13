99e546736b545532482f32b75ef75341f896df61
Wind Yuan <feng.yuan@intel.com>
2017-03-13 02:02:16 -0400

    fix typo of copyright year
     * bug reported by:
       https://github.com/01org/libxcam/issues/410
    
    Signed-off-by: Wind Yuan <feng.yuan@intel.com>

--- xcore/base/xcam_smart_result.h
+++ xcore/base/xcam_smart_result.h
@@ -1,7 +1,7 @@
 /*
  * xcam_smart_result.h - smart result(meta data)
  *
- *  Copyright (c) 206 Intel Corporation
+ *  Copyright (c) 2016-2017 Intel Corporation
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
