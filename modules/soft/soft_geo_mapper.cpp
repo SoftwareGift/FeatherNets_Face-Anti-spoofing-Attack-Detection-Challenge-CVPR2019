fbc06392ebcd4eed52dd3ec6beb8bc37f2d23321
Wind Yuan <feng.yuan@intel.com>
2017-11-09 21:04:11 +0800

    soft-remap: fix pixel out of bound
    Signed-off-by: Wind Yuan <feng.yuan@intel.com>

--- modules/soft/soft_geo_mapper.cpp
+++ modules/soft/soft_geo_mapper.cpp
@@ -227,7 +227,8 @@ GeoMapper::create_soft_geo_mapper ()
 XCamReturn
 XCamSoftTasks::GeoMapTask::work_range (const SmartPtr<Arguments> &base, const WorkRange &range)
 {
-
+    static const Uchar zero_luma_byte[8] = {0, 0, 0, 0, 0, 0, 0, 0};
+    static const Uchar2 zero_uv_byte[4] = {{128, 128}, {128, 128}, {128, 128}, {128, 128}};
     SmartPtr<GeoMapTask::Args> args = base.dynamic_cast_ptr<GeoMapTask::Args> ();
     XCAM_ASSERT (args.ptr ());
     UcharImage *in_luma = args->in_luma.ptr (), *out_luma = args->out_luma.ptr ();
@@ -246,6 +247,12 @@ XCamSoftTasks::GeoMapTask::work_range (const SmartPtr<Arguments> &base, const Wo
     Float2 lut_center ((lut->get_width () - 1.0f) / 2.0f, (lut->get_height () - 1.0f) / 2.0f);
     float x_step = 1.0f / factors.x;
     float y_step = 1.0f / factors.y;
+
+#undef OUT_BOUND
+#define OUT_BOUND(image, first, last) \
+        (in_pos[first].x >= image->get_width ()) ||   \
+        (in_pos[first].y >= image->get_height ()) ||  \
+        (in_pos[last].x <= 0.0f) || (in_pos[last].y <= 0.0f)
 
     for (uint32_t y = range.pos[1]; y < range.pos[1] + range.pos_len[1]; ++y)
         for (uint32_t x = range.pos[0]; x < range.pos[0] + range.pos_len[0]; ++x)
@@ -270,7 +277,10 @@ XCamSoftTasks::GeoMapTask::work_range (const SmartPtr<Arguments> &base, const Wo
             lut->read_interpolate_array<Float2, 8> (lut_pos, in_pos);
             in_luma->read_interpolate_array<float, 8> (in_pos, luma_value);
             convert_to_uchar_N<float, 8> (luma_value, luma_uc);
-            out_luma->write_array_no_check<8> (out_x, out_y, luma_uc);
+            if (OUT_BOUND (in_luma, 0, 7))
+                out_luma->write_array_no_check<8> (out_x, out_y, zero_luma_byte);
+            else
+                out_luma->write_array_no_check<8> (out_x, out_y, luma_uc);
 
             //4x1 UV
             Float2  uv_value[4];
@@ -281,7 +291,10 @@ XCamSoftTasks::GeoMapTask::work_range (const SmartPtr<Arguments> &base, const Wo
             in_pos[3] = in_pos[6] / 2.0f;
             in_uv->read_interpolate_array<Float2, 4> (in_pos, uv_value);
             convert_to_uchar2_N<Float2, 4> (uv_value, uv_uc);
-            out_uv->write_array_no_check<4> (x * 4, y, uv_uc);
+            if (OUT_BOUND (in_uv, 0, 3))
+                out_uv->write_array_no_check<4> (x * 4, y, zero_uv_byte);
+            else
+                out_uv->write_array_no_check<4> (x * 4, y, uv_uc);
 
             //2nd-line luma
             lut_pos[0].y = lut_pos[1].y = lut_pos[2].y = lut_pos[3].y = lut_pos[4].y = lut_pos[5].y =
@@ -289,7 +302,10 @@ XCamSoftTasks::GeoMapTask::work_range (const SmartPtr<Arguments> &base, const Wo
             lut->read_interpolate_array<Float2, 8> (lut_pos, in_pos);
             in_luma->read_interpolate_array<float, 8> (in_pos, luma_value);
             convert_to_uchar_N<float, 8> (luma_value, luma_uc);
-            out_luma->write_array_no_check<8> (out_x, out_y + 1, luma_uc);
+            if (OUT_BOUND (in_luma, 0, 7))
+                out_luma->write_array_no_check<8> (out_x, out_y + 1, zero_luma_byte);
+            else
+                out_luma->write_array_no_check<8> (out_x, out_y + 1, luma_uc);
         }
     return XCAM_RETURN_NO_ERROR;
 }
