6756fc3f5f669c1e9ac5fd034f4874201c72eccd
Yinhang Liu <yinhangx.liu@intel.com>
2018-05-15 20:54:48 +0800

    gl-framework: add GLCommand
     * add uniform operations
     * support uniforming single variable, array,
       vector and matrix
     * support binding buffer

---
+++ modules/gles/gl_command.cpp
@@ -0,0 +1,263 @@
+/*
+ * gl_command.cpp - GL command implementation
+ *
+ *  Copyright (c) 2018 Intel Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Author: Yinhang Liu <yinhangx.liu@intel.com>
+ */
+
+#include "gl_command.h"
+#include "gl_buffer.h"
+
+namespace XCam {
+
+namespace UniformOps {
+
+template <>
+GLenum uniform <GLfloat> (GLint location, GLfloat value)
+{
+    glUniform1f (location, value);
+    return glGetError ();
+}
+
+template <>
+GLenum uniform <GLint> (GLint location, GLint value)
+{
+    glUniform1i (location, value);
+    return glGetError ();
+}
+
+template <>
+GLenum uniform <GLuint> (GLint location, GLuint value)
+{
+    glUniform1ui (location, value);
+    return glGetError ();
+}
+
+template <>
+GLenum uniform_array <GLfloat> (GLint location, const GLfloat *value, GLsizei count)
+{
+    glUniform1fv (location, count, value);
+    return glGetError ();
+}
+
+template <>
+GLenum uniform_array <GLint> (GLint location, const GLint *value, GLsizei count)
+{
+    glUniform1iv (location, count, value);
+    return glGetError ();
+}
+
+template <>
+GLenum uniform_array <GLuint> (GLint location, const GLuint *value, GLsizei count)
+{
+    glUniform1uiv (location, count, value);
+    return glGetError ();
+}
+
+template <>
+GLenum uniform_vect <GLfloat, 2> (GLint location, const GLfloat *value, GLsizei count)
+{
+    glUniform2fv (location, count, value);
+    return glGetError ();
+}
+
+template <>
+GLenum uniform_vect <GLfloat, 3> (GLint location, const GLfloat *value, GLsizei count)
+{
+    glUniform3fv (location, count, value);
+    return glGetError ();
+}
+
+template <>
+GLenum uniform_vect <GLfloat, 4> (GLint location, const GLfloat *value, GLsizei count)
+{
+    glUniform4fv (location, count, value);
+    return glGetError ();
+}
+
+template <>
+GLenum uniform_vect <GLint, 2> (GLint location, const GLint *value, GLsizei count)
+{
+    glUniform2iv (location, count, value);
+    return glGetError ();
+}
+
+template <>
+GLenum uniform_vect <GLint, 3> (GLint location, const GLint *value, GLsizei count)
+{
+    glUniform3iv (location, count, value);
+    return glGetError ();
+}
+
+template <>
+GLenum uniform_vect <GLint, 4> (GLint location, const GLint *value, GLsizei count)
+{
+    glUniform4iv (location, count, value);
+    return glGetError ();
+}
+
+template <>
+GLenum uniform_vect <GLuint, 2> (GLint location, const GLuint *value, GLsizei count)
+{
+    glUniform2uiv (location, count, value);
+    return glGetError ();
+}
+
+template <>
+GLenum uniform_vect <GLuint, 3> (GLint location, const GLuint *value, GLsizei count)
+{
+    glUniform3uiv (location, count, value);
+    return glGetError ();
+}
+
+template <>
+GLenum uniform_vect <GLuint, 4> (GLint location, const GLuint *value, GLsizei count)
+{
+    glUniform4uiv (location, count, value);
+    return glGetError ();
+}
+
+template <>
+GLenum uniform_mat <GLfloat, 2> (GLint location, const GLfloat *value, GLsizei count)
+{
+    glUniformMatrix2fv (location, count, GL_FALSE, value);
+    return glGetError ();
+}
+
+template <>
+GLenum uniform_mat <GLfloat, 3> (GLint location, const GLfloat *value, GLsizei count)
+{
+    glUniformMatrix3fv (location, count, GL_FALSE, value);
+    return glGetError ();
+}
+
+template <>
+GLenum uniform_mat <GLfloat, 4> (GLint location, const GLfloat *value, GLsizei count)
+{
+    glUniformMatrix4fv (location, count, GL_FALSE, value);
+    return glGetError ();
+}
+
+}
+
+GLCmdUniform::GLCmdUniform (const GLchar *name)
+{
+    XCAM_ASSERT (name);
+    strncpy (_name, name, sizeof (_name) - 1);
+}
+
+GLCmdUniform::~GLCmdUniform ()
+{
+}
+
+XCamReturn
+GLCmdUniform::run (GLuint program)
+{
+    GLint location = get_uniform_location (program, _name);
+    XCAM_FAIL_RETURN (ERROR, location >= 0, XCAM_RETURN_ERROR_UNKNOWN, "get_uniform_location failed");
+
+    GLenum error = uniform (location);
+    XCAM_FAIL_RETURN (
+        ERROR, error == GL_NO_ERROR, XCAM_RETURN_ERROR_UNKNOWN,
+        "uniform failed, name:%s error_no:%d", _name, error);
+
+    return XCAM_RETURN_NO_ERROR;
+}
+
+GLint
+GLCmdUniform::get_uniform_location (GLuint program, const GLchar *name)
+{
+    GLint location = glGetUniformLocation (program, name);
+    GLenum error = glGetError ();
+    XCAM_FAIL_RETURN (
+        ERROR, error == GL_NO_ERROR, -1,
+        "get_uniform_location failed, name:%s, error_no:%d", name, error);
+
+    XCAM_FAIL_RETURN (
+        WARNING, location >= 0, -1,
+        "get_uniform_location invalid or unnecessary parameter, name:%s location:%d",
+        name, location);
+
+    return location;
+}
+
+GLCmdBindBufBase::GLCmdBindBufBase (const SmartPtr<GLBuffer> &buf, uint32_t index)
+    : _index (index)
+{
+    XCAM_ASSERT (buf.ptr ());
+    _buf = buf;
+}
+
+GLCmdBindBufBase::~GLCmdBindBufBase ()
+{
+}
+
+XCamReturn
+GLCmdBindBufBase::run (GLuint program)
+{
+    XCAM_UNUSED (program);
+
+    XCamReturn ret = _buf->bind_buffer_base (_index);
+    XCAM_FAIL_RETURN (
+        ERROR, ret == XCAM_RETURN_NO_ERROR, ret,
+        "GLCmdBindBufBase failed, idx:%d", _index);
+
+    return XCAM_RETURN_NO_ERROR;
+}
+
+GLCmdBindBufRange::GLCmdBindBufRange (const SmartPtr<GLBuffer> &buf, uint32_t index)
+    : _index (index)
+    , _offset (0)
+    , _size (0)
+{
+    XCAM_ASSERT (buf.ptr ());
+    _buf = buf;
+
+    const GLBufferDesc &desc = buf->get_buffer_desc ();
+    _size = desc.size;
+}
+
+GLCmdBindBufRange::GLCmdBindBufRange (const SmartPtr<GLBuffer> &buf, uint32_t index, NV12PlaneIdx plane)
+    : _index (index)
+    , _offset (0)
+    , _size (0)
+{
+    XCAM_ASSERT (buf.ptr ());
+    _buf = buf;
+
+    const GLBufferDesc &desc = buf->get_buffer_desc ();
+    _offset = desc.offsets [plane];
+    _size = desc.slice_size [plane];
+}
+
+GLCmdBindBufRange::~GLCmdBindBufRange ()
+{
+}
+
+XCamReturn
+GLCmdBindBufRange::run (GLuint program)
+{
+    XCAM_UNUSED (program);
+
+    XCamReturn ret = _buf->bind_buffer_range (_index, _offset, _size);
+    XCAM_FAIL_RETURN (
+        ERROR, ret == XCAM_RETURN_NO_ERROR, ret,
+        "GLCmdBindBufRange failed, idx:%d", _index);
+
+    return XCAM_RETURN_NO_ERROR;
+}
+
+}
---
+++ modules/gles/gl_command.h
@@ -0,0 +1,205 @@
+/*
+ * gl_command.h - GL command class
+ *
+ *  Copyright (c) 2018 Intel Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Author: Yinhang Liu <yinhangx.liu@intel.com>
+ */
+
+#ifndef XCAM_GL_COMMAND_H
+#define XCAM_GL_COMMAND_H
+
+#include <list>
+#include <gles/gles_std.h>
+
+namespace XCam {
+
+namespace UniformOps {
+
+template <typename TType>
+GLenum uniform (GLint location, TType value);
+
+template <typename TType>
+GLenum uniform_array (GLint location, const TType *value, GLsizei count);
+
+template <typename TType, uint32_t TDim>
+GLenum uniform_vect (GLint location, const TType *value, GLsizei count = 1);
+
+template <typename TType, uint32_t TDim>
+GLenum uniform_mat (GLint location, const TType *value, GLsizei count = 1);
+}
+
+class GLCommand
+{
+public:
+    virtual ~GLCommand () {}
+    virtual XCamReturn run (GLuint program) = 0;
+
+protected:
+    explicit GLCommand () {}
+
+private:
+    XCAM_DEAD_COPY (GLCommand);
+};
+typedef std::list<SmartPtr<GLCommand> > GLCmdList;
+
+class GLCmdUniform
+    : public GLCommand
+{
+public:
+    virtual ~GLCmdUniform ();
+    virtual XCamReturn run (GLuint program);
+
+protected:
+    explicit GLCmdUniform (const GLchar *name);
+
+private:
+    GLint get_uniform_location (GLuint program, const GLchar *name);
+    virtual GLenum uniform (GLint location) = 0;
+
+protected:
+    GLchar        _name[XCAM_GL_NAME_LENGTH];
+};
+
+/* uniform single variable */
+template <typename TType>
+class GLCmdUniformT
+    : public GLCmdUniform
+{
+public:
+    GLCmdUniformT (const GLchar *name, TType value)
+        : GLCmdUniform (name)
+        , _value (value)
+    {}
+    ~GLCmdUniformT () {}
+
+private:
+    virtual GLenum uniform (GLint location) {
+        return UniformOps::uniform <TType> (location, _value);
+    }
+
+private:
+    TType        _value;
+};
+
+/* uniform array: TType array[TCount] */
+template <typename TType, int TCount>
+class GLCmdUniformTArray
+    : public GLCmdUniform
+{
+public:
+    GLCmdUniformTArray (const GLchar *name, const TType *value)
+        : GLCmdUniform (name)
+    {
+        XCAM_ASSERT (value);
+        memcpy (&_value[0], value, sizeof (TType) * TCount);
+    }
+    ~GLCmdUniformTArray () {}
+
+private:
+    virtual GLenum uniform (GLint location) {
+        return UniformOps::uniform_array <TType> (location, _value, TCount);
+    }
+
+private:
+    TType        _value[TCount];
+};
+
+/* uniform vectors: TType vec{TDim}[TCount]*/
+template <typename TType, int TDim, int TCount = 1>
+class GLCmdUniformTVect
+    : public GLCmdUniform
+{
+public:
+    GLCmdUniformTVect (const GLchar *name, const TType *value)
+        : GLCmdUniform (name)
+    {
+        XCAM_ASSERT (value);
+        memcpy (&_value[0], value, sizeof (TType) * TDim * TCount);
+    }
+    ~GLCmdUniformTVect () {}
+
+private:
+    virtual GLenum uniform (GLint location) {
+        return UniformOps::uniform_vect <TType, TDim> (location, _value, TCount);
+    }
+
+private:
+    TType        _value[TDim * TCount];
+};
+
+/* uniform matrix: TType mat{TColumns}x{TRows}[TCount], only support square matrix */
+template <typename TType, int TColumns, int TRows, int TCount = 1>
+class GLCmdUniformTMat
+    : public GLCmdUniform
+{
+public:
+    GLCmdUniformTMat (const GLchar *name, const TType *value)
+        : GLCmdUniform (name)
+    {
+        XCAM_ASSERT (value);
+        memcpy (&_value[0], value, sizeof (TType) * TColumns * TRows * TCount);
+    }
+    ~GLCmdUniformTMat () {}
+
+private:
+    virtual GLenum uniform (GLint location) {
+        XCAM_FAIL_RETURN (
+            ERROR, TColumns == TRows, -1,
+            "uniform_mat only support square matrix, invalid dimension:%dx%d", TColumns, TRows);
+
+        return UniformOps::uniform_mat <TType, TColumns> (location, _value, TCount);
+    }
+
+private:
+    TType        _value[TColumns * TRows * TCount];
+};
+
+class GLBuffer;
+
+class GLCmdBindBufBase
+    : public GLCommand
+{
+public:
+    GLCmdBindBufBase (const SmartPtr<GLBuffer> &buf, uint32_t index);
+    virtual ~GLCmdBindBufBase ();
+
+    virtual XCamReturn run (GLuint program);
+
+private:
+    SmartPtr<GLBuffer>        _buf;
+    uint32_t                  _index;
+};
+
+class GLCmdBindBufRange
+    : public GLCommand
+{
+public:
+    GLCmdBindBufRange (const SmartPtr<GLBuffer> &buf, uint32_t index);
+    GLCmdBindBufRange (const SmartPtr<GLBuffer> &buf, uint32_t index, NV12PlaneIdx plane);
+    virtual ~GLCmdBindBufRange ();
+
+    virtual XCamReturn run (GLuint program);
+
+private:
+    SmartPtr<GLBuffer>        _buf;
+    uint32_t                  _index;
+    uint32_t                  _offset;
+    uint32_t                  _size;
+};
+
+}
+
+#endif // XCAM_GL_COMMAND_H
