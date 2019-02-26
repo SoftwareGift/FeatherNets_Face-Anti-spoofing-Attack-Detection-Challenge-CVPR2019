eba3a1becb4a643ff5e14f41169bde69a7b58e15
SoftwareGift <zhangpengjun1234@163.com>
2019-02-26 15:52:51 +0800

    del other losses

--- face_anti-spoof_challenge@CVPR_zpeng/losses.py
+++ face_anti-spoof_challenge@CVPR_zpeng/losses.py
@@ -5,92 +5,6 @@ from torch import nn
 import torch
 import torch.nn.functional as F
 from torch.autograd import Variable
-
-def weighted_nll_loss(pred, label, weight, avg_factor=None):
-    if avg_factor is None:
-        avg_factor = max(torch.sum(weight > 0).float().item(), 1.)
-    raw = F.nll_loss(pred, label, reduction='none')
-    return torch.sum(raw * weight)[None] / avg_factor
-
-
-def weighted_cross_entropy(pred, label, weight, avg_factor=None,
-                           reduce=True):
-    if avg_factor is None:
-        avg_factor = max(torch.sum(weight > 0).float().item(), 1.)
-    raw = F.cross_entropy(pred, label, reduction='none')
-    if reduce:
-        return torch.sum(raw * weight)[None] / avg_factor
-    else:
-        return raw * weight / avg_factor
-
-
-def weighted_binary_cross_entropy(pred, label, weight, avg_factor=None):
-    if avg_factor is None:
-        avg_factor = max(torch.sum(weight > 0).float().item(), 1.)
-    return F.binary_cross_entropy_with_logits(
-        pred, label.float(), weight.float(),
-        reduction='sum')[None] / avg_factor
-
-
-def sigmoid_focal_loss(pred,
-                       target,
-                       weight,
-                       gamma=2.0,
-                       alpha=0.25,
-                       reduction='elementwise_mean'):
-    pred_sigmoid = pred.sigmoid()
-    pt = (1 - pred_sigmoid) * target + pred_sigmoid * (1 - target)
-    weight = (alpha * target + (1 - alpha) * (1 - target)) * weight
-    weight = weight * pt.pow(gamma)
-    return F.binary_cross_entropy_with_logits(
-        pred, target, weight, reduction=reduction)
-
-
-def weighted_sigmoid_focal_loss(pred,
-                                target,
-                                weight,
-                                gamma=2.0,
-                                alpha=0.25,
-                                avg_factor=None,
-                                num_classes=80):
-    if avg_factor is None:
-        avg_factor = torch.sum(weight > 0).float().item() / num_classes + 1e-6
-    return sigmoid_focal_loss(
-        pred, target, weight, gamma=gamma, alpha=alpha,
-        reduction='sum')[None] / avg_factor
-
-
-def mask_cross_entropy(pred, target, label):
-    num_rois = pred.size()[0]
-    inds = torch.arange(0, num_rois, dtype=torch.long, device=pred.device)
-    pred_slice = pred[inds, label].squeeze(1)
-    return F.binary_cross_entropy_with_logits(
-        pred_slice, target, reduction='elementwise_mean')[None]
-
-
-def smooth_l1_loss(pred, target, beta=1.0, reduction='elementwise_mean'):
-    assert beta > 0
-    assert pred.size() == target.size() and target.numel() > 0
-    diff = torch.abs(pred - target)
-    loss = torch.where(diff < beta, 0.5 * diff * diff / beta,
-                       diff - 0.5 * beta)
-    reduction = F._Reduction.get_enum(reduction)
-    # none: 0, elementwise_mean:1, sum: 2
-    if reduction == 0:
-        return loss
-    elif reduction == 1:
-        return loss.sum() / pred.numel()
-    elif reduction == 2:
-        return loss.sum()
-
-
-def weighted_smoothl1(pred, target, weight, beta=1.0, avg_factor=None):
-    if avg_factor is None:
-        avg_factor = torch.sum(weight > 0).float().item() / 4 + 1e-6
-    loss = smooth_l1_loss(pred, target, beta, reduction='none')
-    return torch.sum(loss * weight)[None] / avg_factor
-
-
 class FocalLoss(nn.Module):
     r"""
         This criterion is a implemenation of Focal Loss, which is proposed in 
@@ -154,38 +68,7 @@ class FocalLoss(nn.Module):
         else:
             loss = batch_loss.sum()
         return loss 
- # https://github.com/clcarwin/focal_loss_pytorch/blob/master/focalloss.py  
-# class FocalLoss(nn.Module):
-#     def __init__(self,device,class_num, gamma=2, alpha=None, size_average=True):
-#         super(FocalLoss, self).__init__()
-#         self.gamma = gamma
-#         self.alpha = alpha
-#         print(alpha)
-#         if isinstance(alpha,(float,int)): self.alpha = torch.Tensor([alpha,1-alpha]).to(device)
-#         if isinstance(alpha,list): self.alpha = torch.Tensor(alpha).to(device)
-#         self.size_average = size_average
 
-#     def forward(self, input, target):
-#         if input.dim()>2:
-#             input = input.view(input.size(0),input.size(1),-1)  # N,C,H,W => N,C,H*W
-#             input = input.transpose(1,2)    # N,C,H*W => N,H*W,C
-#             input = input.contiguous().view(-1,input.size(2))   # N,H*W,C => N*H*W,C
-#         target = target.view(-1,1)
-
-#         logpt = F.log_softmax(input,dim=1)
-#         logpt = logpt.gather(1,target)
-#         logpt = logpt.view(-1)
-#         pt = Variable(logpt.data.exp())
-
-#         if self.alpha is not None:
-#             if self.alpha.type()!=input.data.type():
-#                 self.alpha = self.alpha.type_as(input.data)
-#             at = self.alpha.gather(0,target.data.view(-1))
-#             logpt = logpt * Variable(at)
-
-#         loss = -1 * (1-pt)**self.gamma * logpt
-#         if self.size_average: return loss.mean()
-#         else: return loss.sum()            
 def accuracy(pred, target, topk=1):
     if isinstance(topk, int):
         topk = (topk, )
@@ -217,50 +100,4 @@ class TalyorCrossEntroyLoss(nn.Module):
         loss = talyor_exp.gather(dim=1, index=labels.view(-1,1)).view(-1) /talyor_exp.sum(dim=1)
         loss = loss.mean()
 
-        return loss
-    
-#reference from https://github.com/libuyu/GHM_Detection/mmdetection/mmdet/core/loss/ghm_loss.py    
-class GHMC_Loss:
-    def __init__(self, bins=10, momentum=0.5):
-        self.bins = bins
-        self.momentum = momentum
-        self.edges = [float(x) / bins for x in range(bins+1)]
-        self.edges[-1] += 1e-6
-        if momentum > 0:
-            self.acc_sum = [0.0 for _ in range(bins)]
-
-    def forward(self, input, target, mask):
-        """ Args:
-        input [batch_num, class_num]:
-            The direct prediction of classification fc layer.
-        target [batch_num, class_num]:
-            Binary target (0 or 1) for each sample each class. The value is -1
-            when the sample is ignored.
-        """
-        edges = self.edges
-        mmt = self.momentum
-        weights = torch.zeros_like(input)
-
-        # gradient length
-        g = torch.abs(input.sigmoid().detach() - target)
-
-        valid = mask > 0
-        tot = max(valid.float().sum().item(), 1.0)
-        n = 0  # n valid bins
-        for i in range(self.bins):
-            inds = (g >= edges[i]) & (g < edges[i+1]) & valid
-            num_in_bin = inds.sum().item()
-            if num_in_bin > 0:
-                if mmt > 0:
-                    self.acc_sum[i] = mmt * self.acc_sum[i] \
-                        + (1 - mmt) * num_in_bin
-                    weights[inds] = tot / self.acc_sum[i]
-                else:
-                    weights[inds] = tot / num_in_bin
-                n += 1
-        if n > 0:
-            weights = weights / n
-
-        loss = F.binary_cross_entropy_with_logits(
-            input, target, weights, reduction='sum') / tot
         return loss
