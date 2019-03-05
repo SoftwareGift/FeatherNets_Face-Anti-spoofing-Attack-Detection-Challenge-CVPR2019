4504128e0cef8fa379b767b7310f22bc13c8fa27
SoftwareGift <zhangpengjun1234@163.com>
2019-03-05 11:10:22 +0800

    recode roc.py

--- face_anti-spoof_challenge@CVPR_zpeng/roc.py
+++ face_anti-spoof_challenge@CVPR_zpeng/roc.py
@@ -8,39 +8,25 @@ from sklearn.metrics import roc_curve
 from sklearn.metrics import roc_auc_score
 
 
-#reference from https://github.com/JinghuiZhou/awesome_face_antispoofing/edit/master/roc.py
-def cal_metric(groundTruth, predicted):
-	fpr, tpr, thresholds = roc_curve(groundTruth, predicted)
-	y = (tpr)
-	x = (fpr)
-	z = tpr +fpr
-	tpr = tpr.reshape((tpr.shape[0],1))
-	fpr = fpr.reshape((fpr.shape[0],1))
-	xnew = np.arange(0, 1, 0.00000001)
-	func = interpolate.interp1d(x, y)
-
-	ynew = func(xnew)
-
-	znew = abs(xnew + ynew-1)
-
-	eer=xnew[np.argmin(znew)]
-
-#	print('EER',eer)
-
-	FPR = {"TPR@FPR=10E-2": 0.01, "TPR@FPR=10E-3": 0.001, "TPR@FPR=10E-4": 0.0001}
-
-	TPRs = {"TPR@FPR=10E-2": 0.01, "TPR@FPR=10E-3": 0.001, "TPR@FPR=10E-4": 0.0001}
-	for i, (key, value) in enumerate(FPR.items()):
-
-		index = np.argwhere(xnew == value)
-
-		score = ynew[index] 
-
-		TPRs[key] = float(np.squeeze(score))
-#	    print(key, score)
-	if 0:
-		plt.plot(xnew, ynew)
-
-		plt.show()
-	auc = roc_auc_score(groundTruth, predicted)
-	return eer,TPRs, auc,{'x':xnew, 'y':ynew}
+def cal_metric(target, predicted,show = False):
+    fpr, tpr, thresholds = roc_curve(target, predicted)
+    _tpr = (tpr)
+    _fpr = (fpr)
+    tpr = tpr.reshape((tpr.shape[0],1))
+    fpr = fpr.reshape((fpr.shape[0],1))
+    scale = np.arange(0, 1, 0.00000001)
+    function = interpolate.interp1d(_fpr, _tpr)
+    y = function(scale)
+    znew = abs(scale + y -1)
+    eer = scale[np.argmin(znew)]
+    FPRs = {"TPR@FPR=10E-2": 0.01, "TPR@FPR=10E-3": 0.001, "TPR@FPR=10E-4": 0.0001}
+    TPRs = {"TPR@FPR=10E-2": 0.01, "TPR@FPR=10E-3": 0.001, "TPR@FPR=10E-4": 0.0001}
+    for i, (key, value) in enumerate(FPRs.items()):
+        index = np.argwhere(scale == value)
+        score = y[index] 
+        TPRs[key] = float(np.squeeze(score))
+    auc = roc_auc_score(target, predicted)
+    if show:
+        plt.plot(scale, y)
+        plt.show()
+    return eer,TPRs, auc,{'x':scale, 'y':y}
