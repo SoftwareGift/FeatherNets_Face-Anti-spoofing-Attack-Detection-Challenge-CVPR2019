56c7c5dd6eee9ff201a792e2f12629ea0b041fbc
SoftwareGift <zhangpengjun1234@163.com>
2019-03-28 17:22:54 +0800

    rm all

--- models/__init__.py
+++
@@ -1,5 +0,0 @@
-from .net_factory import *
-from torchvision.models import *
-from .mobilenetv2 import *
-from .FeatherNet import *
-from .MobileLiteNet import *
--- tests/test-xcamsrc-camera.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-gst-launch-1.0 xcamsrc sensor-id=3 capture-mode=1 io-mode=4 ! video/x-raw, format=NV12, width=1920, height=1080, framerate=30/1 ! queue ! vaapiencode_h264 ! fakesink
